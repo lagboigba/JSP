935a046627d06dbb1322b8e1a9b45cdaa45a2278
Kévin Pang <kevinpg94310@gmail.com>
2018-06-18 20:19:41 +0200

    Ajout correction XML et URL

--- Paint/WEB-INF/classes/Paint.java
+++ Paint/WEB-INF/classes/Paint.java
@@ -31,9 +31,9 @@ public class Paint extends HttpServlet {
 
 		public String xml() {
 
-			return "<action type=" + this.type + " startX=" + this.startX + " startY=" + this.startY + " strokeStyle="
-					+ this.strokeStyle + " lineWidth=" + this.lineWidth + " endX=" + this.endX + " endY=" + this.endY
-					+ ">" + "</action>";
+			return "<action type=\"" + this.type + "\" startX=\"" + this.startX + "\" startY=\"" + this.startY + "\" strokeStyle=\""
+					+ this.strokeStyle + "\" lineWidth=\"" + this.lineWidth + "\" endX=\"" + this.endX + "\" endY=\"" + this.endY
+					+ "\">" + "</action>";
 		}
 	}
 
@@ -41,29 +41,58 @@ public class Paint extends HttpServlet {
 	List<Form> figures = new ArrayList<Form>();
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		res.setContentType("text/xml");// setting the content type
-		PrintWriter pw = res.getWriter();// get the stream to write the data
-
-		HttpSession s = req.getSession();
-
+		//res.setContentType("text/xml");// setting the content type
+		res.setContentType("text/xml;charset=UTF-8");
+		//PrintWriter writer = response.getWriter();
 		
-		form1 = new Form(req.getParameter("type"), req.getParameter("startX"), req.getParameter("startY"),
+		PrintWriter pw = res.getWriter();// get the stream to write the data
+		HttpSession s = req.getSession();
+		String xmlSend ="";
+		String xmlLive ="";
+		System.out.println(req.getParameter("type"));
+		if (req.getParameter("type") != null) {
+			form1 = new Form(req.getParameter("type"), req.getParameter("startX"), req.getParameter("startY"),
 					req.getParameter("strokeStyle"), req.getParameter("lineWidth"), req.getParameter("endX"),
 					req.getParameter("endY"));
 
-		if (req.getParameter("endDraw").equals("true")) {
+			if (req.getParameter("endDraw").equals("true")) {
 				figures.add(form1);
+			}else {
+				
+				form1 = null;
 			}
 
-
+		}
+		
+		xmlSend +="<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+//		xmlSend +="<picture>";
+//		if(!figures.isEmpty())
+//		for (int i = 0; i < figures.size(); i++) {
+//			xmlSend +=figures.get(i).xml();
+//		}
+//		if(form1 != null) {
+//		xmlSend += form1.xml();
+//		}
+//		xmlSend +="</picture>";
+//		
+//		pw.write(xmlSend);
+//		pw.flush();
+//		pw.close();
+//		xmlSend ="";
+		pw.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		pw.println("<picture>");
+		if(!figures.isEmpty())
 		for (int i = 0; i < figures.size(); i++) {
 			pw.println(figures.get(i).xml());
 		}
+		if(form1 != null) {
 		pw.println(form1.xml());
+		}
 		pw.println("</picture>");
-
+		figures = null;
+		pw.flush();
 		pw.close();
+		
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
